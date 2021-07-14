01b12467160075afab3d88663973c4913fed1950
Nathaniel Griswold <nategriswold@gmail.com>
2021-07-13 21:56:25 -0500

    blah

--- flake.nix
+++ flake.nix
@@ -1,13 +1,4 @@
 {
    inputs.dwarffs.url = "github:edolstra/dwarffs";
-#  inputs.dwarffs.url = "git+file:///home/nwg/project/dwarffs";
-
-#  inputs.nix.url = "github:NixOS/nix";
-
-#  inputs.dwarffs.inputs.nix.follows = "nix";
-#  inputs.dwarffs.inputs.nixpkgs.follows = "nix/nixpkgs";
-
-  outputs = { self, ... }: {
-
-  };
+   outputs = { self, ... }: {};
 }
